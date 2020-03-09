f8ee430bc96452b057f91f08f99a1cfe03f98ab2
LostCity Offical <lostcity@users.noreply.github.com>
2020-03-09 14:07:52 +0800

    Backup-1
    This is only backup!
    And change some system options.
    No any updates!

--- star.bat
+++ star.bat
@@ -1,5 +1,5 @@
 @echo off
 :server
-java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UseLargePagesInMetaspace -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar paper-128.jar nogui
+java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UseLargePagesInMetaspace -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar paper-129.jar nogui
 pause
 goto server
